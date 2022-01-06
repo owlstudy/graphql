9bd5dd47b4ce1744a924414104e9222eb9d8c5e5
hyunjin <leehj0110@kakao.com>
2022-01-06 23:25:48 +0900

    chore: add database entities option

--- grapql-server/server/src/index.ts
+++ grapql-server/server/src/index.ts
@@ -1,28 +1,32 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import cors from "cors";
+import { schema } from "./Schema";
 import { createConnection } from "typeorm";
+import { Users } from "./Entities/Users";
 
 const main = async () => {
   await createConnection({
     type: "mysql",
     database: "GraphqlCRUD",
     username: "root",
-    password: "",
+    password: "root",
     logging: true,
     synchronize: false,
+    entities: [Users],
   });
 };
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-// app.use(
-//   "/graphql",
-//   graphqlHTTP({
-//     graphiql: true,
-//   })
-// );
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 app.listen(3001, () => {
   console.log("Server started at port 3001");
 });
